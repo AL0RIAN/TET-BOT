e983164125239930bdb9e8dae1f05f2c8aaa74b7
Kirill <slowpok19450509@gmail.com>
2022-08-09 17:16:14 +0300

    1. Field response_properties added 2. Function low_price added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 # DiplomaTestSkillbox token
 bot = telebot.TeleBot("5374577409:AAEqaoAS1vPRc1mDhre5aM3Z1rIw1Ln_yug")
@@ -13,5 +14,25 @@ headers = {
     "X-RapidAPI-Key": "624162d6f6msh896925e99832f85p116bfbjsn1aa5f4b17c97",
     "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
 }
+
+# Response Properties
+response_properties = {"sortOrder": "", "city": "", "hotelCount": 0, "picture": False, "pictureCount": 0}
+
+
+@bot.message_handler(commands=["lowprice"])
+def low_price(message: types.Message) -> None:
+    """
+    This function sets functions route to find cheapest hotels
+
+    :param message: message /lowprice
+    :return: None
+    """
+
+    # The search will be done by price (from cheap to expensive)
+    response_properties["sortOrder"] = "PRICE"
+
+    msg = bot.reply_to(message, "Введите название города")
+    # bot.register_next_step_handler(msg, get_name)
+
 
 bot.polling(none_stop=True, interval=0)
