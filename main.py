d294d9ebc21a84091c1449d9a8647ad56159cc7b
Kirill <slowpok19450509@gmail.com>
2022-08-09 18:18:00 +0300

    Function get_name added
    Light changes:
    1. Function low_price documentation updated
    2. Russian words translated into English

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import telebot
 from telebot import types
 
@@ -24,15 +25,41 @@ def low_price(message: types.Message) -> None:
     """
     This function sets functions route to find cheapest hotels
 
-    :param message: message /lowprice
+    Function sets sortOrder properties to 'Price' (from cheap to expensive)
+    And waits for a message from user with city name. Then it transmits control to
+    Function get_name.
+
+    :param message: Message instance with text '/lowprice'
     :return: None
     """
 
-    # The search will be done by price (from cheap to expensive)
+    # Search will be done by price (from cheap to expensive)
     response_properties["sortOrder"] = "PRICE"
 
-    msg = bot.reply_to(message, "Введите название города")
-    # bot.register_next_step_handler(msg, get_name)
+    msg = bot.reply_to(message, "Enter city name:")
+    bot.register_next_step_handler(msg, get_name)
+
+
+def get_name(message: types.Message) -> None:
+    """
+    This function gets city name from user
+    And waits for a message from user with number of hotels in this city.
+    It then checks message text on correctness and transmits control to function get_number.
+
+    :param message: Message instance with city name
+    :return: None
+    """
+
+    try:
+        if not re.fullmatch(pattern=r"[ A-Za-z]+", string=f"{message.text}"):
+            raise ValueError
+    except ValueError:
+        bot.reply_to(message=message, text="<b>Error</b>: Incorrect value", parse_mode="html")
+        print("\nError: User input incorrect value\n")
+    else:
+        print(f"\nInfo: User input {message.text}\n")
+        msg = bot.reply_to(message, "Enter number of hotels:")
+        # bot.register_next_step_handler(msg, get_number)
 
 
 bot.polling(none_stop=True, interval=0)
